4b4013f73bc92857cc2d2cc7400f47888e3019b3
mohamedhussain827 <mohamedhussain8207@gmail.com>
2026-09-04 22:23:26 +0530

    Delete server directory

--- server/src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Notice-to-Action API running on http://localhost:${PORT}`);
-});
--- server/src/utils/id.js
+++
@@ -1,8 +0,0 @@
-const { v4: uuidv4 } = require('uuid');
-
-/** e.g. shortId('u') -> "u_9f1c2a3b" */
-function shortId(prefix) {
-  return `${prefix}_${uuidv4().split('-')[0]}`;
-}
-
-module.exports = { shortId };
